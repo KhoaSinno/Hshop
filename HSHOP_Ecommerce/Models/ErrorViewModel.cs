92e9f714a135b6c62563a5c34ddb1306f2b0fdfd
Sinoo <khoasinno@gmail.com>
2024-08-28 22:04:28 +0700

    init pj

---
+++ HSHOP_Ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HSHOP_Ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

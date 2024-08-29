3ab191b2700666501ef00b87b6484fcf7e126e68
Sinoo <khoasinno@gmail.com>
2024-08-29 08:27:15 +0700

    ViewComponent and menu

---
+++ HSHOP_Ecommerce/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using HSHOP_Ecommerce.Data;
+using HSHOP_Ecommerce.ViewModals;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HSHOP_Ecommerce.ViewComponents
+{
+    public class MenuLoaiViewComponent : ViewComponent
+    {
+        private readonly Hshop2023Context db;
+        public MenuLoaiViewComponent(Hshop2023Context context) { 
+        db = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Loais.Select(menu => new MenuLoaiVM
+            {
+               MaLoai = menu.MaLoai, TenLoai = menu.TenLoai, SoLuong = menu.HangHoas.Count
+            });
+            return View(data); // auto ref Default.cshtml
+        }
+    }
+}
---
+++ HSHOP_Ecommerce/ViewModals/MenuLoaiVM.cs
@@ -0,0 +1,9 @@
+﻿namespace HSHOP_Ecommerce.ViewModals
+{
+    public class MenuLoaiVM
+    {
+        public int MaLoai {  get; set; }
+        public string TenLoai { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
